34316f144f25d23003a7e636b7ebbd7c54842272
Eric Truong <pengu@erics-mbp-2.lan>
2020-02-17 23:20:27 -0800

    First commit

---
+++ Boba.java
@@ -0,0 +1,20 @@
+public class Boba {
+
+    private String name;
+
+    private String place;
+
+    private String date;
+
+    // perfect, okay, bad
+    // powdery, watery, too sweet, not sweet
+    private String rateDrink;
+
+    private String reviewer;
+
+    // if not perfect; hard, too soft, slimy
+    private String rateBoba;
+
+
+
+}
