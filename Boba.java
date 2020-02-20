cd4d5ab98279691b65a548973b6669ff4d8db183
Eric Truong <pengu@dhcp-39-37-35.dhcpint.csulb.edu>
2020-02-20 09:01:29 -0800

    Finished boba drink

--- Boba.java
+++ Boba.java
@@ -15,6 +15,49 @@ public class Boba {
     // if not perfect; hard, too soft, slimy
     private String rateBoba;
 
+    public Boba (String a, String b, String c, String d, String e, String f){
 
+        name = a;
+
+        place = b;
+
+        date = c;
+
+        rateDrink = d;
+
+        reviewer = e;
+
+        rateBoba = f;
+    }
+
+    public String getName(){
+
+        return name;
+    }
+
+    public String getPlace(){
+
+        return place;
+    }
+
+    public String getDate(){
+
+        return date;
+    }
+
+    public String getRateDrink(){
+
+        return rateDrink;
+    }
+
+    public String getReviewer(){
+
+        return reviewer;
+    }
+
+    public String getRateBoba(){
+
+        return rateBoba;
+    }
 
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main (String[] args){
+
+        createReview();
+
+
+    }
+
+    public static void createReview(){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Name of drink: ");
+
+        String name = scan.nextLine();
+
+        System.out.print("\n Name of shop: ");
+
+        String place = scan.nextLine();
+
+        System.out.print("\n Date: ");
+
+        String date = scan.nextLine();
 
 
     }
